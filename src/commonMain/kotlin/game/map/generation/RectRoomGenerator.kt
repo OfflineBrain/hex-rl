39dcdc5b202cfd125fefa2e4161746414a7a1042
OfflineBrain <leonidivakhnenko@gmail.com>
2022-08-10 20:20:30 +0900

    initial states

---
+++ src/commonMain/kotlin/game/map/generation/RectRoomGenerator.kt
@@ -0,0 +1,9 @@
+package game.map.generation
+
+import game.map.generation.rule.*
+
+class RectRoomGenerator(private val config: GenerationConfig) {
+
+
+
+}
--- src/commonMain/kotlin/game/map/generation/rule/GenerationConfig.kt
+++ src/commonMain/kotlin/game/map/generation/rule/GenerationConfig.kt
@@ -1,11 +1,12 @@
 package game.map.generation.rule
 
-import game.map.generation.*
-import kotlinx.serialization.*
+import game.map.generation.CellType
+import kotlinx.serialization.Serializable
 
 @Serializable
 data class GenerationConfig(
     val size: RoomSize,
     val generations: Int,
+    val initialStates: Map<CellType, Double>,
     val rules: List<Rule>
 )
