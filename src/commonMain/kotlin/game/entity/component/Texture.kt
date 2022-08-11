56e8f22bf39746e5af7357e9efade32be823e284
OfflineBrain <leonidivakhnenko@gmail.com>
2022-08-12 01:05:02 +0900

    Textures

---
+++ src/commonMain/kotlin/game/entity/component/Texture.kt
@@ -0,0 +1,5 @@
+package game.entity.component
+
+import com.offlinebrain.ecs.Component
+
+data class Texture(val id: Int, val storage: Int) : Component
---
+++ src/commonMain/kotlin/game/view/TextureContainer.kt
@@ -0,0 +1,76 @@
+package game.view
+
+import com.soywiz.korge.view.fast.*
+import com.soywiz.korma.geom.*
+import hex.*
+import kotlin.math.*
+
+class TextureContainer private constructor(
+    maxSize: Int, val id: Int,
+    private val boardSize: PointInt,
+    private val tileSize: PointInt,
+) : FSprites(maxSize) {
+    private val rowShift = tileSize.y * (3.0 / 4.0)
+
+    var shift: PointInt = PointInt(0, 0)
+        private set
+
+    operator fun <R> invoke(id: Int, fn: FSprite.() -> R): R {
+        return FSprite(id).fn()
+    }
+
+    fun alloc(fn: FSprite.() -> Unit): Int {
+        return alloc().also(fn).id
+    }
+
+    fun FSprite.setPosition(x: Float, y: Float) {
+        setPos(
+            x = (x + shift.x) * tileSize.x,
+            y = (y + shift.y) * tileSize.y
+        )
+    }
+
+    fun shift(x: Int, y: Int) {
+        var shiftX = x
+        var shiftY = y
+        val new = PointInt(x + shift.x, y + shift.y)
+        if (new.x > 5) {
+            shiftX = 5 - shift.x
+        } else if (new.x < -boardSize.x + 1) {
+            shiftX = -boardSize.x + 1 - shift.x
+        }
+
+        if (new.y > 10) {
+            shiftY = 10 - shift.y
+        } else if (new.y < -boardSize.y + 1) {
+            shiftY = -boardSize.y + 1 - shift.y
+        }
+
+        shift = PointInt(shiftX + shift.x, shiftY + shift.y)
+
+        fastForEach {
+            it.x += (tileSize.x * shiftX).toFloat()
+            it.y += (rowShift * shiftY).toFloat()
+        }
+    }
+
+    fun getHex(xy: Point): Hex {
+        val y = xy.y
+        val x = ((xy.x + ((shift.x % 2) * (tileSize.x / 2)) - tileSize.x / 2) / tileSize.x)
+        val t1: Double = y / (tileSize.y / 2)
+        val t2: Double = floor(x + t1)
+        val r: Double = floor((floor(t1 - x) + t2) / 3)
+        val q: Double = floor((floor(2 * x + 1) + t2) / 3) - r
+        return Hex(q.toInt() - shift.x + (shift.y / 2), r.toInt() - shift.y)
+    }
+
+    companion object {
+        private val textureContainer = mutableMapOf<Int, TextureContainer>()
+
+        operator fun invoke(maxSize: Int, id: Int, boardSize: PointInt, tileSize: PointInt): TextureContainer {
+            return textureContainer.getOrPut(id) { TextureContainer(maxSize, id, boardSize, tileSize) }
+        }
+
+        operator fun get(id: Int): TextureContainer? = textureContainer[id]
+    }
+}
